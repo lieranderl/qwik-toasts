71f686eb4b2c01861d56b65154d49005d21b2b7e
lieranderl <19622412+lieranderl@users.noreply.github.com>
2024-01-03 18:04:41 +0100

    v0.0.4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/qwik-toasts/**/*.{js,jsx,ts,tsx}"
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {},
